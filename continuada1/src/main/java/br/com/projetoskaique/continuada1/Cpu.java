d7fbc48ad38454bd476fbd334cd24be4ab31b3be
KaiqueFigu1 <kaique.lucena@bandtec.com.br>
2020-09-06 05:37:52 -0300

    Terminando a continuada

---
+++ continuada1/src/main/java/br/com/projetoskaique/continuada1/Cpu.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projetoskaique.continuada1;
+public class Cpu {
+    Double maximo = 0.0;
+    Double minimo = 5.0;
+    Double media = 0.0;
+}
---
+++ continuada1/src/main/java/br/com/projetoskaique/continuada1/Memoria.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projetoskaique.continuada1;
+public class Memoria {
+    Integer maximo = 0;
+    Integer minimo = 8001;
+    Double media = 0.0;
+}
